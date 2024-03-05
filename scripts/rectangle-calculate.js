228e4619eb87696013960aa004c65cd9376410c2
IstiyakIfti <istiyakifti04@gmail.com>
2024-03-05 23:59:18 +0600

    rectangle Function Added

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,3 +1,22 @@
 function CalculateTriangleArea(){
-    console.log('button clicked');
+    // get rectangle base value
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthText);
+    // console.log(length);
+
+    // get rectangle 
+    const rectangleWeightInput = document.getElementById('rectangle-weight');
+    const rectangleWeightText = rectangleWeightInput.value;
+    const weight = parseFloat(rectangleWeightText);
+    // console.log(weight);
+
+    // calculate rectangle area
+    const area = length * weight;
+    console.log('area of the rectangle:', area);
+
+    // display rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
+
 }
